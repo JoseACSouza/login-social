062077468cacf0e1902c20b8a9d795d478eb3b52
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2024-02-07 12:03:12 -0300

    req 08

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val email:TextInputEditText by lazy { findViewById(R.id.email_text_input) }
+    private val emailError: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
     private val password:TextInputEditText by lazy { findViewById(R.id.password_text_input) }
     private val submit:Button by lazy { findViewById(R.id.login_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        submit.setOnClickListener(this)
         email.addTextChangedListener {
             submit.isEnabled = email.text.toString().isNotEmpty() && password.text.toString()
                 .isNotEmpty()
@@ -25,7 +29,20 @@ class MainActivity : AppCompatActivity() {
             submit.isEnabled = email.text.toString().isNotEmpty() && password.text.toString()
                 .isNotEmpty()
         }
-
+    }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.login_button -> {
+                val emailRegex = "^[A-Za-z0-9.]+@[A-Za-z]+.[A-Za-z]+"
+                val validateEmail = email.text.toString().matches(emailRegex.toRegex())
+                if (!validateEmail){
+                    emailError.error = "Email inválido"
+                } else {
+                    emailError.error = null
+                }
+                return
+            }
+        }
     }
 
 }
